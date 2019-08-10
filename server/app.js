c5fe06e1a283bac28bceb7681b2fcb8aa664cce2
andrew <andrewjthomsen@yahoo.com>
2019-08-10 15:01:30 -0700

    Moved repo content from starter code repo after completing assignment to current repository. All tests passing and am ready to turn in

--- server/app.js
+++ server/app.js
@@ -1,8 +1,50 @@
-const express = require('express');
-
-
+const express = require("express");
 const app = express();
+const axios = require("axios");
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const cache = {};
+
+app.get("/", (req, res) => {
+  let iReq = req.query.i;
+  let tReq = req.query.t;
+
+  if (iReq !== undefined) {
+    if (cache.hasOwnProperty(iReq) === true) {
+      res.send(cache[iReq]);
+    } else {
+      axios
+        .get("http://www.omdbapi.com/?i=" + iReq + "&apikey=8730e0e")
+        .then(response => {
+          cache[iReq] = response.data;
+          res.send(response.data);
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(error.response);
+          }
+        });
+    }
+  } else {
+    tReq = tReq.replace(" ", "%20");
+    if (cache.hasOwnProperty(tReq) === true) {
+      res.send(cache[tReq]);
+    } else {
+      axios
+        .get("http://www.omdbapi.com/?t=" + tReq + "&apikey=8730e0e")
+        .then(response => {
+          cache[tReq] = response.data;
+          res.send(response.data);
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(error.response);
+          }
+        });
+    }
+  }
+});
 
 module.exports = app;
